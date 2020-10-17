0018bd1e6088f80f2a16d3406091ea0c4d04e933
andrasimion99 <61377543+andrasimion99@users.noreply.github.com>
2020-10-17 21:45:40 +0300

    first commit

---
+++ Assets/Scripts/AnimController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    public GameObject objectToAnimate;
+    void Update()
+    {
+        if (Input.GetButtonDown("1Key"))
+        {
+            objectToAnimate.GetComponent<Animator>().Play("FirstTry");
+        }
+        if (Input.GetButtonDown("2Key"))
+        {
+            objectToAnimate.GetComponent<Animator>().Play("Rock'nRoll");
+        }
+        if (Input.GetButtonDown("3Key"))
+        {
+            objectToAnimate.GetComponent<Animator>().Play("Foarfec");
+        }
+    }
+}
